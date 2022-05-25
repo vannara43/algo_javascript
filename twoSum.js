84884040369f2e6ab770f33ac853e90cd2ddf2e3
vannara43 <36809428+vannara43@users.noreply.github.com>
2022-05-24 18:24:09 -0700

    update twoSum

--- twoSum.js
+++ twoSum.js
@@ -2,7 +2,9 @@
 //target: 10
 //output: [-1,11]
 
-//method 1
+//time complexity: O(N^2)
+//Space Complexity: O(1)
+//method 1: nested for loops | quadratic time
 function twoSum(array, target) {
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array.length; j++) {
@@ -13,15 +15,13 @@ function twoSum(array, target) {
 
     }
 }
-//time complexity: O(N^2)
-//Space Complexity: O(1)
 
 
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20];
 console.log("method 1: ", twoSum(array, 8));
 
 //hash table is used to keep track of what you already seen
-//method 2: using hash table
+//using hash table
 function twoSum2() {
     let nums = {};
     for (let i = 0; i < array.length; i++) {
@@ -30,9 +30,9 @@ function twoSum2() {
     return nums
 }
 
-console.log("method 2: ", twoSum2(array, 8));
+console.log("hash table: ", twoSum2(array, 8));
 
-//method 3
+//method 2
 function twoSum3(array, target) {
     let nums = {};
     for (const num of array) {
@@ -46,4 +46,33 @@ function twoSum3(array, target) {
     return [];
 }
 
-console.log("method 3 :", twoSum3(array, 8));
+console.log("method 2 :", twoSum3(array, 8));
+
+//two pointer method
+function twoSum4(array, target) {
+    let i = 0;
+    let j = array.length - 1;
+    array = array.sort((a, b) => a - b);
+
+    while (i < j) {
+        let sum = array[i] + array[j];
+
+        if (sum === target) {
+            return [array[i], array[j]];
+        } else if (sum < target) {
+            i++;
+        } else if (sum > target) {
+            j--
+        }
+    }
+}
+
+console.log("two pointer method :", twoSum4(array, 18));
+
+//edge cases
+
+//big O, the amount of time it iterate through to find a solution, the time it takes function to process, the space complexity increases
+
+//O(1) is constant time
+//O(N) if array, 1 array = 1 sec, if 2 arr then 2 sec
+//O(N^2) quadratic time
