49a337c6602ea0267a433d44dd986a7f7c58a072
vannara43 <36809428+vannara43@users.noreply.github.com>
2022-08-12 18:28:48 -0700

    added info to AVeryBigSum

--- aVeryBigSum.js
+++ aVeryBigSum.js
@@ -1,4 +1,9 @@
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20];
+//return the sum of all array elements
+
+let array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20];
+let array2 = [40, 200, 30, 40, 50, 60, 70, 80, 90, 1000, 105, 220];
+let array3 = [-1000, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200];
+let array4 = [100000, 20000, 3000, 4000, 5000000, 60000, 700000, 80000, 9000000, 1000, 15000, 200000];
 
 function aVeryBigSum(array) {
     let totalSum = 0;
@@ -8,4 +13,9 @@ function aVeryBigSum(array) {
     return totalSum;
 }
 
-console.log(aVeryBigSum(array))
+console.log({
+    Case1: aVeryBigSum(array1),
+    Case2: aVeryBigSum(array2),
+    Case3: aVeryBigSum(array3),
+    Case4: aVeryBigSum(array4),
+});
